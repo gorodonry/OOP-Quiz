cff7bb7474d4d67f6a9118a891634c0d15316b7f
ryangordon11235 <78880773+ryangordon11235@users.noreply.github.com>
2022-08-08 16:40:29 +1200

    Set up solution.
    Solution uses Prism, an application framework for WPF.

---
+++ OOPQuiz/OOPQuiz/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace OOPQuiz.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "OOP Quiz";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
