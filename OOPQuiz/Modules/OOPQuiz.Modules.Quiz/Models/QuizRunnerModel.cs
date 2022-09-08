529b485619ea91d71a1a040b5b7aa88fe766e232
ryangordon11235 <78880773+ryangordon11235@users.noreply.github.com>
2022-09-08 22:17:05 +1200

    Questions set up for the quiz runner model.
    Upon navigation to the quiz runner view, the model will no import and select questions (according to the specified category) from the question service.

--- OOPQuiz/Modules/OOPQuiz.Modules.Quiz/Models/QuizRunnerModel.cs
+++ OOPQuiz/Modules/OOPQuiz.Modules.Quiz/Models/QuizRunnerModel.cs
@@ -1,4 +1,8 @@
 ﻿using OOPQuiz.Business.Models;
+using System.Collections.Generic;
+using OOPQuiz.Services.Interfaces;
+using System;
+using System.Diagnostics;
 
 namespace OOPQuiz.Modules.Quiz.Models
 {
@@ -7,6 +11,73 @@ namespace OOPQuiz.Modules.Quiz.Models
     /// </summary>
     public class QuizRunnerModel
     {
-        
+        protected const int numberOfQuestions = 10;
+
+        protected Dictionary<string, List<IQuestion>> allQuestions;
+
+        protected string _questionCategory;
+        protected List<IQuestion> _questions = new();
+        protected int _currentQuestionNumber = 1;
+
+        /// <summary>
+        /// Creates a new instance of the quiz runner.
+        /// </summary>
+        public QuizRunnerModel()
+        {
+
+        }
+
+        /// <summary>
+        /// The category of questions asked by this instance of the quiz runner.
+        /// </summary>
+        public string QuestionCategory
+        {
+            get { return _questionCategory; }
+            set { _questionCategory = value; }
+        }
+
+        /// <summary>
+        /// The number of the current question.
+        /// </summary>
+        public int CurrentQuestionNumber
+        {
+            get { return _currentQuestionNumber; }
+        }
+
+        /// <summary>
+        /// Provides the model with a list of questions.
+        /// </summary>
+        /// <param name="questions">Dictionary of all questions, obtained from <see cref="IQuestionService"/>.</param>
+        public void ImportQuestions(Dictionary<string, List<IQuestion>> questions)
+        {
+            if (allQuestions is null)
+                allQuestions = questions;
+        }
+
+        /// <summary>
+        /// Sets up the list of questions to use in the quiz.
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        public void SelectQuestions()
+        {
+            if (allQuestions.ContainsKey(QuestionCategory))
+            {
+                List<IQuestion> questionPool = allQuestions[QuestionCategory];
+
+                // Randomly select the desired number of questions.
+                for (int i = 0; i < numberOfQuestions; i++)
+                {
+                    IQuestion question = questionPool[new Random().Next(0, questionPool.Count)];
+
+                    _questions.Add(question);
+
+                    questionPool.Remove(question);
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid question category: {QuestionCategory}");
+            }
+        }
     }
 }
--- OOPQuiz/Modules/OOPQuiz.Modules.Quiz/ViewModels/QuizRunnerViewModel.cs
+++ OOPQuiz/Modules/OOPQuiz.Modules.Quiz/ViewModels/QuizRunnerViewModel.cs
@@ -2,6 +2,9 @@
 using Prism.Regions;
 using OOPQuiz.Services.Interfaces;
 using OOPQuiz.Modules.Quiz.Models;
+using OOPQuiz.Business.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace OOPQuiz.Modules.Quiz.ViewModels
 {
@@ -9,9 +12,34 @@ namespace OOPQuiz.Modules.Quiz.ViewModels
     {
         private QuizRunnerModel _model = new();
 
+        /// <summary>
+        /// The category of questions asked by this instance of the quiz runner.
+        /// </summary>
+        public string QuestionCategory
+        {
+            get { return _model.QuestionCategory; }
+        }
+
+        /// <summary>
+        /// The number of the current question.
+        /// </summary>
+        public int CurrentQuestionNumber
+        {
+            get { return _model.CurrentQuestionNumber; }
+        }
+
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
-            
+            // Determine the question category, and from that the questions.
+
+            // This will be the case once the menu has been set up
+            // _model.QuestionCategory = navigationContext.Parameters.GetValue<string>("QuestionCategory");
+
+            _model.QuestionCategory = "C#";
+
+            _model.SelectQuestions();
+
+            RaisePropertyChanged(nameof(QuestionCategory));
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
@@ -31,6 +59,8 @@ namespace OOPQuiz.Modules.Quiz.ViewModels
         public QuizRunnerViewModel(IRegionManager regionManager, IQuestionService questionService)
         {
             _regionManager = regionManager;
+
+            _model.ImportQuestions(questionService.GetQuestions());
         }
     }
 }
