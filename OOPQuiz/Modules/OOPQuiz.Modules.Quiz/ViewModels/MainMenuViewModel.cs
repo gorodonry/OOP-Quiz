4caf72d1ce458652faf1dbcfde3fe17882e96e2d
ryangordon11235 <78880773+ryangordon11235@users.noreply.github.com>
2022-10-03 21:57:04 +1300

    Question category selection now available
    Instead of progressing straight to the quiz, the application now opens to the main menu, where the user can select a question category they would like to take a quiz in.

--- OOPQuiz/Modules/OOPQuiz.Modules.Quiz/ViewModels/MainMenuViewModel.cs
+++ OOPQuiz/Modules/OOPQuiz.Modules.Quiz/ViewModels/MainMenuViewModel.cs
@@ -5,13 +5,14 @@ using OOPQuiz.Modules.Quiz.Views;
 using OOPQuiz.Services.Interfaces;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Prism.Commands;
 
 namespace OOPQuiz.Modules.Quiz.ViewModels
 {
     /// <summary>
     /// View model for the main menu.
     /// </summary>
-    public class MainMenuViewModel : BindableBase, INavigationAware
+    public class MainMenuViewModel : BindableBase, INavigationAware, IRegionMemberLifetime
     {
         private readonly List<string> _questionCategories;
 
@@ -20,18 +21,55 @@ namespace OOPQuiz.Modules.Quiz.ViewModels
         /// </summary>
         public List<string> QuestionCategories => _questionCategories;
 
-        public void OnNavigatedTo(NavigationContext navigationContext)
-        {
-            RaisePropertyChanged(nameof(QuestionCategories));
+        private string _selectedQuestionCategory;
 
-            // This is a placeholder until proper navigation from the main menu is set up.
+        /// <summary>
+        /// The question category the user has selected.
+        /// </summary>
+        public string SelectedQuestionCategory
+        {
+            get { return _selectedQuestionCategory; }
+            set
+            {
+                SetProperty(ref _selectedQuestionCategory, value);
+
+                StartQuiz.RaiseCanExecuteChanged();
+            }
+        }
+
+        private DelegateCommand _startQuiz;
+        public DelegateCommand StartQuiz =>
+            _startQuiz ?? (_startQuiz = new DelegateCommand(ExecuteStartQuiz, CanExecuteStartQuiz));
+
+        /// <summary>
+        /// Starts a quiz with questions in the category selected by the user.
+        /// </summary>
+        /// <remarks>
+        /// This is done by navigating to a new instance of <see cref="QuizRunner"/>.
+        /// </remarks>
+        void ExecuteStartQuiz()
+        {
             var parameters = new NavigationParameters
             {
                 { "OnNavigatedTo", "Initialise Quiz" },
-                { "QuestionCategory", "Python" }
+                { "QuestionCategory", SelectedQuestionCategory }
             };
 
             _regionManager.RequestNavigate(RegionNames.ContentRegion, nameof(QuizRunner), parameters);
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether or not the user can start the quiz.
+        /// </summary>
+        /// <returns>True if the user has selected a question category, otherwise false.</returns>
+        bool CanExecuteStartQuiz()
+        {
+            return !string.IsNullOrEmpty(SelectedQuestionCategory);
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            RaisePropertyChanged(nameof(QuestionCategories));
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
@@ -43,6 +81,8 @@ namespace OOPQuiz.Modules.Quiz.ViewModels
         {
             
         }
+
+        public bool KeepAlive => false;
 
         private readonly IRegionManager _regionManager;
 
